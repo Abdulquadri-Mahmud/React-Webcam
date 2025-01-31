8ecd361f05c68c2c989049f91a2ad57e0762fa84
Abdulquadri-Mahmud <abdulquadrimahmud06@gmail.com>
2025-01-31 16:28:25 +0100

    changes made

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import React from 'react'
-import WebcamToggle from '../Webcam'
+import WebcamToggle from './Webcam2'
+import Webcam1 from './Webcam1'
 
-export default function Routes() {
+export default function AppRoutes() {
   return (
     <Router>
         <Routes>
+          <Route path='/' element={<Webcam1/>}/>
           <Route path='/webcam' element={<WebcamToggle/>}/>
         </Routes>
     </Router>
---
+++ src/Webcam1.jsx
@@ -0,0 +1,40 @@
+import { useRef, useState } from "react";
+import { Link } from "react-router-dom";
+import Webcam from "react-webcam";
+
+
+export default function Webcam1() {
+    const webRef = useRef(null);
+    const [img, setImg] = useState(null);
+    const [isCameraOn, setIsCameraOn] = useState(false);
+  
+    const videoConstraints = {
+      width: 300,
+      height: 300,
+      facingMode: "user",
+    };
+  
+    const showImage = () => {
+      setImg(webRef.current.getScreenshot());
+    }
+  
+    const handleDelete = () => {
+      setImg(null);
+    }
+  
+    return (
+      <>
+        <p>React Webcam</p>
+  
+        <Link to={'/webcam'}>Second webcam</Link>
+  
+        <Webcam ref={webRef} videoConstraints={videoConstraints} audio={false}/>
+  
+        <button onClick={() => {showImage()}}>Show Image in console</button>
+        <div className="img">
+          <img src={img} alt="image" />
+        </div>
+        <button onClick={() => handleDelete()}>Delete Image</button>
+      </>
+    )
+}
